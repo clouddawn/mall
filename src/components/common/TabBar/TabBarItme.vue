b859a59f7c3aec072ec8cfa87ed6a967ce807f01
clouddawn <1993456181@qq.com>
2022-07-20 16:52:18 +0800

    尝试首页上拉加载更多é前备份

---
+++ src/components/common/TabBar/TabBarItme.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="tabbar-item" @click="itemClick">
+    <slot v-if="!isActive" name="tabbar-item-icon"></slot>
+    <slot v-else name="tabbar-item-active-icon"></slot>
+    <div class="tabbar-text" :style="activeStyle">
+      <slot name="tabbar-item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItme",
+  data() {
+    return {
+      options: ["首页首页首页首页首页", "分类", "购物车", "我的"],
+      currentIndex: 0,
+    };
+  },
+  props: {
+    link: String,
+    activeColor: {
+      type: String,
+      default: "red",
+    }
+  },
+  methods: {
+    select(currentIndex) {
+      this.currentIndex = currentIndex;
+    },
+    itemClick() {
+      this.$router.push(this.link);
+    }
+  },
+  computed: {
+    isActive() {
+      // console.log(this.$route.path.indexOf(this.link));
+      // console.log(this.link);
+      // console.log(this.$route.path.indexOf(this.path));
+      return this.$route.path.indexOf(this.link) > -1;
+    },
+    activeStyle() {
+      return this.isActive ? {color: this.activeColor} : {};
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.active {
+  color: red;
+}
+
+.tabbar-item {
+  flex: 1;
+  text-align: center;
+  font-size: 14px;
+  vertical-align: middle;
+
+  .tabbar-text > div {
+    font-size: 12px;
+    color: inherit;
+  }
+
+  img {
+    width: 24px;
+    height: 24px;
+    margin-top: 4px;
+  }
+}
+</style>
---
+++ src/components/content/Tabbar.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="tabbar">
+    <tab-bar-itme link="/home" activeColor="red">
+      <template #tabbar-item-icon>
+        <img src="~@/assets/img/tabbar/home.svg" alt="">
+      </template>
+      <template #tabbar-item-active-icon>
+        <img src="~@/assets/img/tabbar/home_active.svg" alt="">
+      </template>
+      <template #tabbar-item-text>
+        <div>首页</div>
+      </template>
+    </tab-bar-itme>
+    <tab-bar-itme link="/category">
+      <template #tabbar-item-icon>
+        <img src="@/assets/img/tabbar/category.svg" alt="">
+      </template>
+      <template #tabbar-item-active-icon>
+        <img src="@/assets/img/tabbar/category_active.svg" alt="">
+      </template>
+      <template #tabbar-item-text>
+        <div>分类</div>
+      </template>
+    </tab-bar-itme>
+    <tab-bar-itme link="/shopcart">
+      <template #tabbar-item-icon>
+        <img src="@/assets/img/tabbar/shopcart.svg" alt="">
+      </template>
+      <template #tabbar-item-active-icon>
+        <img src="@/assets/img/tabbar/shopcart_active.svg" alt="">
+      </template>
+      <template #tabbar-item-text>
+        <div>购物车</div>
+      </template>
+    </tab-bar-itme>
+    <tab-bar-itme link="/profile">
+      <template #tabbar-item-icon>
+        <img src="@/assets/img/tabbar/profile.svg" alt="">
+      </template>
+      <template #tabbar-item-active-icon>
+        <img src="@/assets/img/tabbar/profile_active.svg" alt="">
+      </template>
+      <template #tabbar-item-text>
+        <div>我的</div>
+      </template>
+    </tab-bar-itme>
+  </div>
+</template>
+
+<script>
+import TabBarItme from "components/common/TabBar/TabBarItme.vue";
+
+export default {
+  components: {TabBarItme},
+  data() {
+    return {};
+  },
+  mounted() {
+
+  },
+  methods: {
+    select(currentIndex) {
+      this.currentIndex = currentIndex;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.tabbar {
+  background-color: #f5f5f5;
+  display: flex;
+  justify-content: space-around;
+  height: 49px;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  box-shadow: 0 -1px 1px rgba(145, 145, 145, 0.15);
+}
+</style>
